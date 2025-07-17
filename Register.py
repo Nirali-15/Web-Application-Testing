90b7331d0879db6323887e8a664baa7d653532e2
Nirali-15 <niralibhanvadiya73@gmail.com>
2025-07-17 14:27:09 +0530

    commit

---
+++ Register.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+serv_obj = Service(r"C:\Driver\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("http://localhost:3000/signup")
+
+driver.find_element(By.NAME, "fullName").send_keys("admin")
+driver.find_element(By.NAME, "username").send_keys("Nirali")
+driver.find_element(By.NAME, "email").send_keys("admin@gmail.com")
+driver.find_element(By.NAME, "phone").send_keys("9876543210")
+driver.find_element(By.NAME, "password").send_keys("admin@123")
+driver.find_element(By.NAME, "confirmPassword").send_keys("admin@123")
+driver.find_element(By.NAME, "acceptTerms").click()
+
+driver.find_element(By.XPATH, "//button[contains(text(), 'Sign Up')]").click()
+
+time.sleep(5)
+driver.close()
